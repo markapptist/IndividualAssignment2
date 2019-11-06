214f0e5fc7044a9c502ea6ff81d5fe5d5d81bdde
Mark <mark@theapptist.com>
2019-11-06 17:15:14 -0500

    Intial commit

---
+++ Mail Pilot/Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Bonus - make this class a Singleton!
+
+[System.Serializable]
+public class BulletPoolManager : MonoBehaviour
+{
+    public GameObject bullet;
+
+    //TODO: create a structure to contain a collection of bullets
+    public int bulletAmount = 50;
+    static Queue<GameObject> bullets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO: add a series of bullets to the Bullet Pool
+        bullets = new Queue<GameObject>();
+
+        for(int i = 0; i < bulletAmount; i++)
+        {
+            GameObject bulletShoot = Instantiate(bullet);
+            bulletShoot.SetActive(false);
+            bullets.Enqueue(bulletShoot);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(bullets.Count);
+    }
+
+    //TODO: modify this function to return a bullet from the Pool
+    public GameObject GetBullet(Vector3 pos, Quaternion id)
+    {
+
+        GameObject bulletGet = bullets.Dequeue();
+        bulletGet.SetActive(true);
+        bulletGet.transform.position = pos;
+        bulletGet.transform.rotation = id;
+        return bulletGet;
+    }
+
+    //TODO: modify this function to reset/return a bullet back to the Pool 
+    public void ResetBullet(GameObject bullet)
+    {
+        bullets.Enqueue(bullet);
+        bullet.SetActive(true);
+    }
+}
